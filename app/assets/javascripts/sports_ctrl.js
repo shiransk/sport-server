e2d5f75e504e065746a9761170182ee4fd8e5254
Shiran Koriat <shiransk@gmail.com>
2016-09-04 10:46:31 -0700

    add name toggle

--- app/assets/javascripts/sports_ctrl.js
+++ app/assets/javascripts/sports_ctrl.js
@@ -2,20 +2,31 @@
   "use strict";
   angular.module("app").controller("sports_ctrl", function($scope, $http) {
     
-   $scope.setup = function () {
+  $scope.setup = function () {
     $http.get("/api/v1/sports.json").then(function(response) {
-      $scope.sports = response.data
-      $scope.test = true
-
-      $scope.flip = function () {
-       $scope.test = !$scope.test;
-      }
-
-
+      $scope.sports = response.data;
     });
-   }
+  }
 
 
-    window.$scope = $scope;
+  $scope.flip = function (sport) {
+    sport.bioVis = !sport.bioVis
+  }
+
+
+  $scope.addNewSport = function(sport_name, age, ball) {
+    var sport = {
+      name: sport_name,
+      age: age,
+      ball: ball
+    };
+    $scope.sports.push(sport);
+    $scope.name = null;
+    $scope.age = " ";
+    $scope.ball = " ";
+  }
+
+  window.$scope = $scope;
+
   });
 }());
